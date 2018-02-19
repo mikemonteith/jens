826ab34d17020df3fa65b83581389399d42ef4d1
Mike Monteith <mike@mikemonteith.com>
2018-02-19 17:22:57 +0000

    Add some common shortcuts copy/paste etc.

--- windowMiddleware.js
+++ windowMiddleware.js
@@ -19,6 +19,34 @@ function createWindow (config, dispatch) {
         click: function() {
           dispatch({type: 'OPEN_DIALOG'})
         },
+      }, {
+        type: "separator",
+      }, {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        selector: 'undo:',
+      }, {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+X',
+        selector: 'redo:',
+      }, {
+        type: "separator",
+      }, {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        selector: 'cut:',
+      }, {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        selector: 'copy:',
+      }, {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        selector: 'paste:',
+      }, {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        selector: 'selectAll:',
       }]
     }
   ])
