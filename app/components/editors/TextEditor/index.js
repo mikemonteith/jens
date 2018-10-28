cccc76a851c2e1cdd059ca2b6d79bc26bae5d25e
Mike Monteith <mike@mikemonteith.com>
2018-10-28 13:58:00 +0000

    Add some text editors

---
+++ app/components/editors/TextEditor/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Editor, EditorState, ContentState, RichUtils} from 'draft-js'
+
+require('draft-js/dist/Draft.css')
+
+export default class TextEditor extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createWithContent(
+        ContentState.createFromText(props.content)
+      ),
+    }
+  }
+
+  onChange(editorState) {
+    this.setState({ editorState })
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  render() {
+    return (
+      <Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange.bind(this)}
+        handleKeyCommand={this.handleKeyCommand}
+      />
+    )
+  }
+
+}
--- app/containers/FilesWindow/index.js
+++ app/containers/FilesWindow/index.js
@@ -1,5 +1,10 @@
+import fs from 'fs'
+import path from 'path'
 
 import React from 'react'
+
+import MarkdownEditor from '../../components/editors/MarkdownEditor'
+import TextEditor from '../../components/editors/TextEditor'
 
 require('./style.scss')
 
@@ -7,30 +12,44 @@ import connect from '../../connect'
 import { updateFileTree } from './actions'
 import { selectFileTree } from './selectors'
 
-const renderNode = (node, i) => {
+const renderNode = (props, i) => {
+  const node = props.node
   if (!node) {
     return null
   }
 
   if(node.type === 'directory') {
-    return <Dir key={i} node={node} />
+    return <Dir key={i} node={node} onDoubleClick={props.onDoubleClick} />
   } else if(node.type === 'file') {
-    return <File key={i} node={node} />
+    return <File key={i} node={node} onDoubleClick={props.onDoubleClick} />
   }
 }
 
 const Dir = props => (
   <li className="files-window-directory">
-    <p className="filies-window-directory__name">{props.node.name}</p>
+    <p
+      onDoubleClick={props.onDoubleClick.bind(this, props.node)}
+      className="filies-window-directory__name"
+    >
+      {props.node.name}
+    </p>
     <ol className="files-window-directory__children">
-      {props.node.children.map(renderNode)}
+      {props.node.children.map((child, i) => {
+        props = {node: child, onDoubleClick: props.onDoubleClick}
+        return renderNode(props, i)
+      })}
     </ol>
   </li>
 )
 
 const File = props => (
   <li className="files-window-file">
-    <p className="files-window-file__name">{props.node.name}</p>
+    <p
+      className="files-window-file__name"
+      onDoubleClick={props.onDoubleClick.bind(this, props.node)}
+    >
+      {props.node.name}
+    </p>
   </li>
 )
 
@@ -39,12 +58,75 @@ class FilesWindow extends React.Component {
     this.props.updateFileTree()
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: null,
+    }
+  }
+
+  handleValueChange = mdeState => {
+    this.setState({ mdeState })
+  }
+
+  onDoubleClick = node => {
+    if(node.type === 'file') {
+      //TODO: async
+      const content = fs.readFileSync(node.path, "utf8")
+      this.setState({
+        filepath: node.path,
+        content,
+      })
+    } else if(node.type === 'directory') {
+      // TODO: toggle open/close the directory
+    }
+  }
+
+  renderEditor() {
+    const { filepath } = this.state
+    if (!filepath) {
+      return null
+    }
+
+    const ext = path.extname(filepath).toLowerCase()
+
+    switch (ext) {
+      case '.md':
+      case '.markdown':
+        return (
+          <MarkdownEditor
+            id={this.state.filepath}
+            content={this.state.content}
+          />
+        )
+      case '.json':
+        // TODO
+        break;
+      case '.html':
+      default:
+        return (
+          <TextEditor
+            id={this.state.filepath}
+            content={this.state.content}
+          />
+        )
+    }
+  }
+
   render() {
     return (
       <div className="files-window">
-        <ol>
-          {renderNode(this.props.tree)}
-        </ol>
+        <div className="files-window-filetree">
+          <ol>
+            {renderNode({
+              node: this.props.tree,
+              onDoubleClick: this.onDoubleClick.bind(this),
+            })}
+          </ol>
+        </div>
+        <div className="files-window-editor">
+          {this.renderEditor()}
+        </div>
       </div>
     )
   }
